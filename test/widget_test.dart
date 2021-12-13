1fc0884afb76e079c16acfe3a84006fe8b4fd6bd
Kento-Ishizaki <kentoron1115@gmail.com>
2021-12-13 18:31:07 +0900

    Fix: テスト修正(失敗テスト)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,9 +6,8 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutter_ci_cd/main.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
@@ -25,6 +24,6 @@ void main() {
 
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('3'), findsOneWidget);
   });
 }
